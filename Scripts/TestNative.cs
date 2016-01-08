d90233027e9f1d85b9d48ddc6183fc0b842b3fb9
fredz0003 <ic3balandran@yahoo.com>
2016-01-08 08:20:57 -0600

    Initial commit
    Tested managed DLL with unity3D. Confirmed communication with PLCSim.

---
+++ Scripts/TestNative.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using myS7ProSimLib;
+
+public class TestNative : MonoBehaviour {
+
+    /*
+    * Placed s7wspsm.dll in visual studio directory
+    * Used tlbimp.exe to generate library DLL
+    * place new generated DLL on assets/plugins
+    * also note that the DLL is treated as managed code
+    */
+    public S7ProSimClass ps;
+    public bool input_0_0;
+    
+
+    void Start () {
+        ps = new S7ProSimClass();
+
+        ps.Connect();
+        print("State " + ps.GetState());
+        ps.SetScanMode(ScanModeConstants.ContinuousScan);
+
+        // Here we pass the ref as an obj, since WriteInputPoint method
+        // can take bit, word, dwords, as addresses ref obj can take the
+        // for of bool, int, float, etc.
+        object refInput0_0 = input_0_0;
+                                        
+        ps.WriteInputPoint(0, 0, ref refInput0_0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
